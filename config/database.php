6209f202f2ea4b40c198eb20a0ef2c1474a9910d
Dante Bradshaw <plansuperior@gmail.com>
2023-05-25 17:52:44 -0400

    configuring db connection thru docker containers

--- config/database.php
+++ config/database.php
@@ -46,8 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5505'),
+            'host' => env('DB_HOST', 'laravel-app-mysql'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'password'),

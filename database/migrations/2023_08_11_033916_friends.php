5ec7a7c4e1e11925b0fd731cbf95c7f923c82052
dbradshaw <dante@d3corp.com>
2023-08-10 23:46:05 -0400

    add friends table migration

---
+++ database/migrations/2023_08_11_033916_friends.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('friend_id');
+            $table->timestamps();
+
+            // Define foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+
+            // Add a unique constraint to ensure no duplicate friendships
+            $table->unique(['user_id', 'friend_id']);
+        });    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friends');
+    }
+};

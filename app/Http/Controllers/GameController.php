345272cd97bffb3b2d2f6e24a3e7225591086ffb
Dante Bradshaw <plansuperior@gmail.com>
2023-11-03 21:02:13 -0400

    add back validation

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -63,16 +63,16 @@ class GameController extends Controller
     }
     public function createGame(Request $request)
     {
-        // $request->validate([
-        //     'time' => ['required', 'string', 'max:30'],
-        //     'date' => ['required', 'string', 'max:10'],
-        //     'state' => ['required', 'string', 'max:3'],
-        //     'country' => ['required', 'string', 'max:3'],
-        //     'power_level' => ['required', 'integer', 'max:10', 'min:1'],
-        //     'number_players' => ['required', 'integer'],
-        //     'format' => ['required', 'string'],
-        //     'description' => ['nullable', 'string', 'min:10', 'max:200'],
-        // ]);
+        $request->validate([
+            'time' => ['required', 'string', 'max:30'],
+            'date' => ['required', 'string', 'max:10'],
+            'state' => ['required', 'string', 'max:3'],
+            'country' => ['required', 'string', 'max:3'],
+            'power_level' => ['required', 'integer', 'max:10', 'min:1'],
+            'number_players' => ['required', 'integer'],
+            'format' => ['required', 'string'],
+            'description' => ['nullable', 'string', 'min:10', 'max:200'],
+        ]);
 
         $newGame = Games::create([
             'time' => request()->input('time'),

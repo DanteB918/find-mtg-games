2915d23ec292975aa499beadd156c890655d9aac
Dante Bradshaw <plansuperior@gmail.com>
2023-11-03 21:25:54 -0400

    some more home blade text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,11 +14,15 @@
                         </div>
                     @endif
                     <div class="row">
-                    {{ __('You are logged in!') }}
+                        You are logged in!
                     </div>
                     <div class="row">
-                    {{ __('Games:') }}
-                    <p><a href = "{{ route('games') }}">Click Here</a> to see all games.</p>
+                        Friends:
+                        <p>See who's online! <a href="{{ route('friendslist') }}">Friend List</a></p>
+                    </div>
+                    <div class="row">
+                        Games:
+                        <p>to see all games, <a href = "{{ route('games') }}">Click Here</a>.</p>
                     </div>
                     
                 </div>
